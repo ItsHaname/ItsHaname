a684fa232af82632c2a0053e3159ca9443d317a4
a_b_Hanane <angelhananehanane@gmail.com>
2025-02-13 20:22:28 +0100

    test

---
+++ exe1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int a, b, sum;
+
+    std::cout << "Enter two integers: ";
+    std::cin >> a >> b;
+
+    sum = a + b;
+
+    std::cout << "Sum: " << sum << std::endl;
+
+    return 0;
+}
